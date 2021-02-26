7e4c78212418123850083129e84fcb951f886323
u9g <43508353+u9g@users.noreply.github.com>
2021-02-26 12:47:05 -0500

    Create inventorySlots.js

---
+++ inventorySlots.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Crafting slots
+  0: [307, 55],
+  1: [195, 35],
+  2: [231, 35],
+  3: [195, 71],
+  4: [231, 71],
+
+  5: [15, 15], // Helmet
+  9: [15, 167], // Inventory
+  18: [15, 203], // Inventory
+  27: [15, 239], // Inventory
+  36: [15, 283], // Tool bar
+  45: [153, 123] // Left hand
+}
